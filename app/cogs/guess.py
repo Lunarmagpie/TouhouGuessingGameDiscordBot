e30248f61fe69e697f31e2818aef17595215f748
Drakomire <Bambolambo0@gmail.com>
2021-08-01 16:36:05 -0400

    id fixed

--- app/cogs/guess.py
+++ app/cogs/guess.py
@@ -32,7 +32,7 @@ class Guess(commands.Cog):
 
     @commands.command()
     async def vote(self,ctx,*args):
-        has_voted = True if requests.get("https://top.gg/api/bots/869410048743473182/check?userId=318039587412901890").json() >= 1 else False
+        has_voted = True if requests.get(f"https://top.gg/api/bots/869410048743473182/check?userId={ctx.author.id}").json() >= 1 else False
         await ctx.channel.send(f"{has_voted}Please vote for Touhou Character Guesser! Voting helps Touhou Character Guesser have more visibility.\nhttps://top.gg/bot/869410048743473182/vote")
 
     @commands.command()
