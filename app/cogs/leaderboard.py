48a2c356968d98c1d0455b9caf18d1582498a670
Benji42 <63879236+Benji42@users.noreply.github.com>
2021-08-01 16:35:57 -0400

    Update leaderboard.py

--- app/cogs/leaderboard.py
+++ app/cogs/leaderboard.py
@@ -11,27 +11,30 @@ class Leaderboard(commands.Cog):
     @commands.command()
     async def leaderboard(self,ctx,*args):
         # player_info = scoreboard.get_player_information(ctx.author)
-        res = scoreboard.table.find().sort("score",-1).limit(25)
+        res = scoreboard.table.find().sort("score",-1).limit(20)
 
-        out_name = ""
-        out_score = ""
+        out = ""
 
-        for user in res:
+        for i,user in enumerate(res):
             #Can remove once its confirmed that all top 20 players have a username
             try:
                 name = user['username']
             except:
                 name = "None"
 
-            out_name += (f"**{name}**\t\n")
-            out_score += (f"{user['score']} points\n")
+            place_medal = f"{i+1}."
+            if i == 0: place_medal = ":first_place:"
+            if i == 1: place_medal = ":second_place:"
+            if i == 2: place_medal = ":third_place:"
+
+            out += (f"{place_medal} **{name}** - {user['score']} points\n")
+            if i == 2: out += "\n"
 
         embed = discord.Embed(
-            title = "Top 25 Players",
+            title = "Top 20 Players",
             color = 0xfcba03
         )
-        embed.add_field(name='​', value=out_name, inline=True)
-        embed.add_field(name='​', value=out_score, inline=True)
+        embed.add_field(name='​', value=out, inline=True)
         await ctx.channel.send(embed=embed)
 
 def setup(bot: "Bot"):
