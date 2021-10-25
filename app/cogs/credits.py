3ae43ecad7fd28e6dae1f99dc10ed68cf1b5b3e2
Ben <ben@pop-os.localdomain>
2021-10-24 23:03:19 -0400

    Credits menu visuals updated & useless code deleted

--- app/cogs/credits.py
+++ app/cogs/credits.py
@@ -9,32 +9,31 @@ class Credits(commands.Cog):
 
     @commands.command()
     async def credits(self,ctx,*args):
-        player_info = scoreboard.get_player_information(ctx.author)
-
-        res = scoreboard.table.find().sort("score",-1).limit(25)
-
-        out = ""
-        for player in res:
-            user = await self.bot.fetch_user(player["player_id"])
-
-            out += (f"{user.name}: {player['score']}\n")
-        
         embed = discord.Embed(
             # title = "💮 Credits",
             color = 0xfcba03,
-            description=f"""
-           **Artists**:
-                dairi: https://www.pixiv.net/en/users/4920496\nkaoru: https://www.pixiv.net/en/users/743845
-           **Development**:
-                <@!318039587412901890>
-                <@!318076068290494466>
-                
-                Please contact either of us if you have any questions, or want to request removal of your data. We'll try to respond as soon as possible!
-                
-            **Source**:
-                If you want to view the source code or contribute to the project, it is available at https://github.com/Lunarmagpie/TouhouGuessingGameDiscordBot
-            """
+            description=""
         )
+        embed.add_field(name="Artists", value=
+            """
+            dairi: https://www.pixiv.net/en/users/4920496
+            kaoru: https://www.pixiv.net/en/users/743845
+            baba: https://www.pixiv.net/en/users/3422465
+            """, 
+        inline = False)
+        embed.add_field(name="Development", value=
+            """
+            <@!318039587412901890>
+            <@!318076068290494466>
+                    
+            Please contact either of us if you have any questions, or want to request removal of your data. We'll try to respond as soon as possible!
+            """, 
+        inline = False)
+        embed.add_field(name="Source", value=
+            """
+            If you want to view the source code or contribute to the project, it is available at https://github.com/Lunarmagpie/TouhouGuessingGameDiscordBot
+            """, 
+        inline = False)
         embed.set_author(name="Credits", icon_url="https://b.catgirlsare.sexy/GmYW8Bs86OFs.png")
         await ctx.channel.send(embed=embed)
 
